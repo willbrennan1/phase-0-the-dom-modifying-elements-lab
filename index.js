846b2cab840ba9f03767f3494ef36349d491c535
Will Brennan <willbrennan314@gmail.com>
2021-11-08 09:04:38 -0500

    commit tree 9a5ca57c2866208690d75b0e085be7c697137ab5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Will is the champion";
+document.body.appendChild(newheader);
+
+// document.body.append(newHeader);
+// newHeader.nodeName = 'H1';
+// // newHeader.id = 'victory';
+// console.log(newHeader.id);
+// let message = newHeader.textContent = "YOUR-NAME is the champion";
+
